de13b701d8525755c55b4b55dd792b77a1e92ff6
Renan Almeida <125520953+RenanGBA@users.noreply.github.com>
2026-04-15 18:25:54 -0300

    Adicionado calculadora de desconto (#1)

---
+++ exemplos_aula/src/calculoCupomDesconto.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class calculoCupomDesconto {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        double valor, desconto;
+
+        System.out.println("Qual o valor do produto?");
+        valor = scanner.nextDouble();
+        System.out.println("Qual o valor do desconto?");
+        desconto = scanner.nextDouble();
+        scanner.close();
+        System.out.println("O valor do produto com desconto é: R$" + (valor - (valor*(desconto/100))));
+    }
+}
